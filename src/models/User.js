768beb0809a46d4645aa85957fc193d18ebb5bbb
Roseane Gama <roseanecavalcantee@hotmail.com>
2023-07-20 16:22:21 -0300

    add comments

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,13 @@
 const { Model, DataTypes } = require("sequelize");
 
+/* overwirting the init method from Model.
+in this case, all we did was:
+1. already pass in the structure so we don't have to do it when initializing the model
+2. pass the sequelize connection as the only paramater
+
+why?
+just to make the code more modular and clean
+*/
 class User extends Model {
   static init(sequelize) {
     super.init(
